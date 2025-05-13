ca983b99a37669b64e9b736b03ec40ca121991eb
LeTanThuan <letanthuan@dtu.edu.vn>
2025-05-14 00:10:52 +0700

    Uppdate Khung

---
+++ SanBongBayNguoi.cpp
@@ -0,0 +1,15 @@
+#include "SanBongBayNguoi.h"
+#include <iostream>
+
+SanBongBayNguoi::SanBongBayNguoi()
+{
+
+}
+
+SanBongBayNguoi::SanBongBayNguoi(string loaiSan,
+                string soluongCotLuoi, string docaoCuaLuoi, string matSan, string mucChongTham,
+                string coThamTruot, string soLuongVot, string soLuongCau,string coPhongKhoiDong,
+                string loaivotPhuHop, string coPhongNghi, string soluong_NguoiToiDa, string lichsu_SuDung, string mucdo_HuHong,string donvi_QuanLy)
+{
+    
+}
--- SanBongBayNguoi.h
+++ SanBongBayNguoi.h
@@ -1,5 +1,5 @@
 #ifndef SBBayNguoi_H
-#define SB#ifndef SanCauLong_H
+#define SanCauLong_H
 #define SanCauLong_H
 
 #include <HeThongSanTheThao.h>
@@ -8,11 +8,31 @@ using namespace std;
 class SanBongBayNguoi:public HeThongSanTheThao
 {
 private:
+    string loaiSan;
+    string soluongCotLuoi;
+    string docaoCuaLuoi;
+    string matSan;
+    string mucChongTham;
+    string coThamTruot;
+    string soLuongVot;
+    string soLuongCau;
+    string coPhongKhoiDong;
+    string loaivotPhuHop;
+    string coPhongNghi;
+    string soluong_NguoiToiDa;
+    string lichsu_SuDung;
+    string mucdo_HuHong;
+    string donvi_QuanLy;
     
 
 public:
+SanBongBayNguoi();
+SanBongBayNguoi(string loaiSan,
+                string soluongCotLuoi, string docaoCuaLuoi, string matSan, string mucChongTham,
+                string coThamTruot, string soLuongVot, string soLuongCau,string coPhongKhoiDong,
+                string loaivotPhuHop, string coPhongNghi, string soluong_NguoiToiDa, string lichsu_SuDung, string mucdo_HuHong,string donvi_QuanLy);
 
-virtual void  output();
+virtual void  input();
 
 virtual void output();
     
